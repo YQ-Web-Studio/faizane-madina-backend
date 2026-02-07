17ee96007b767d08c8d8b242e932d08eedb3ff93
MohammedYusufQureshi <yusufq2004@gmail.com>
2026-02-07 20:35:50 +0000

    Force HTTPS url

--- config/server.ts
+++ config/server.ts
@@ -3,6 +3,7 @@ import 'dotenv/config';
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: 'https://portal.faizanemadinasouthend.co.uk',
   proxy: true,
   app: {
     keys: env.array('APP_KEYS'),
--- dist/config/server.js
+++ dist/config/server.js
@@ -4,6 +4,7 @@ require("dotenv/config");
 exports.default = ({ env }) => ({
     host: env('HOST', '0.0.0.0'),
     port: env.int('PORT', 1337),
+    url: 'https://portal.faizanemadinasouthend.co.uk',
     proxy: true,
     app: {
         keys: env.array('APP_KEYS'),
